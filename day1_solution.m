61c1705746f803e9dc819293f642102a3aba4cb7
ross-wilkinson <ross.wilkinson@icloud.com>
2020-12-10 00:06:30 -0700

    solve day 1 part 2

--- day1_solution.m
+++ day1_solution.m
@@ -2,6 +2,13 @@
 
 expenseReport = table2array(readtable('day1_input.txt'));
 
-diff = 2020 - expenseReport;
-C = intersect(expenseReport,diff);
-sol = C(1) * C(2)
+%% Part 1
+diff1 = 2020 - expenseReport;
+C1 = intersect(expenseReport,diff1);
+sol1 = C1(1) * C1(2)
+
+%% Part 2
+A = bsxfun(@plus,expenseReport,expenseReport');
+diff2 = 2020-A;
+C2 = intersect(expenseReport,diff2);
+sol2 = C2(1) * C2(2) * C2(3)
