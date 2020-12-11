548f0aacdbd6e2cbca216d3af6837d62f45bd917
ross-wilkinson <ross.wilkinson@icloud.com>
2020-12-10 17:22:14 -0700

    solve day 10 part 1

---
+++ adapterChain.m
@@ -0,0 +1,15 @@
+function [sol, singles, triples] = adapterChain(adapters)
+% Finds adapter chain required to connect source to charging outlet
+
+%% Part 1
+
+sorted = sort(adapters);
+diffs = diff(sorted);
+
+singles = sum(diffs == 1) + 1;
+triples = sum(diffs == 3) + 1;
+
+sol = singles * triples
+
+end
+
